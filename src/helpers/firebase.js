29c0a42f0ea01c46bfd59a23b4697291d569331f
Carson-Key <keyc@oregonstate.edu>
2022-01-25 11:40:16 -0800

    Init Firebase SDK

---
+++ src/helpers/firebase.js
@@ -0,0 +1,14 @@
+// Packages
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhi2yY3AIrVGPt_3Wb4ssPUR3fvbe8-aA",
+  authDomain: "brokengm-bee41.firebaseapp.com",
+  projectId: "brokengm-bee41",
+  storageBucket: "brokengm-bee41.appspot.com",
+  messagingSenderId: "510631621580",
+  appId: "1:510631621580:web:219f880e5417383a2ee369"
+}
+const app = initializeApp(firebaseConfig)
+
+export default app
