7f924a5a67b7ae953b910fd4093ba6a2d880bd14
Carson-Key <keyc@oregonstate.edu>
2022-01-28 13:16:11 -0800

    Add firestore get for initial timer data

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -7,10 +7,11 @@ const Timer = (props) => {
     const { 
         timer, 
         setTimer,
-        isActive
+        isActive,
+        isLoading
     } = props
 
-    const [dividedTimer, setDividedTimer] = useState([])
+    const [dividedTimer, setDividedTimer] = useState(splitNumberToReverseArray(timer))
 
     const getTimerSection = (indexOne, indexTwo) => {
         return (
@@ -20,31 +21,40 @@ const Timer = (props) => {
     }
 
     useEffect(() => {
-        if (timer === 0) {
-            setDividedTimer([])
-        }
-        let interval = null
-        if (isActive) {
-          interval = setInterval(() => {
-            setTimer(timer => timer + 1)
-            setDividedTimer(splitNumberToReverseArray(timer))
-          }, 10)
-        } else if (!isActive && timer !== 0) {
-            clearInterval(interval)
-        }
-        return () => clearInterval(interval)
-      }, [isActive, timer, setTimer])
-
-    return (
-        <h1>
-            {
-                getTimerSection(6, 7) + ":" + 
-                getTimerSection(4, 5) + ":" + 
-                getTimerSection(2, 3) + ":" + 
-                getTimerSection(0, 1)
+        if (!isLoading) {
+            if (timer === 0) {
+                setDividedTimer([])
+            } 
+            if (!isLoading) {
+                setDividedTimer(splitNumberToReverseArray(timer))
             }
-        </h1>
-	)
+            let interval = null
+            if (isActive) {
+              interval = setInterval(() => {
+                setTimer(timer => timer + 1)
+                setDividedTimer(splitNumberToReverseArray(timer))
+              }, 10)
+            } else if (!isActive && timer !== 0) {
+                clearInterval(interval)
+            }
+            return () => clearInterval(interval)
+        }
+      }, [isActive, timer, setTimer, isLoading])
+
+    if (isLoading) {
+        return (<h1>Loading...</h1>)
+    } else {
+        return (
+            <h1>
+                {
+                    getTimerSection(6, 7) + ":" + 
+                    getTimerSection(4, 5) + ":" + 
+                    getTimerSection(2, 3) + ":" + 
+                    getTimerSection(0, 1)
+                }
+            </h1>
+        )
+    }
 }
 
 export default Timer
--- src/pages/Clock.js
+++ src/pages/Clock.js
@@ -1,12 +1,25 @@
 // Packages
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 // Components
 import Container from "../components/Container"
 import Timer from "../components/Timer"
+// Helpers
+import { getDocument } from "../helpers/firestore"
 
 const Clock = () => {
+    const [isLoading, setIsLoading] = useState(true)
     const [timer, setTimer] = useState(0)
     const [isActive, setIsActive] = useState(false)
+
+    async function getClockData() {
+        let clockData = await (await getDocument("clocks", "b37722f7-00da-4d7c-b9f5-67325445c313")).data()
+        setTimer(clockData.timer)
+        setIsLoading(false)
+    }
+
+    useEffect(() => {
+        getClockData()
+    }, [])
 
     return (
         <Container>
@@ -14,8 +27,9 @@ const Clock = () => {
                 timer={timer} 
                 setTimer={setTimer}
                 isActive={isActive}
+                isLoading={isLoading}
             />
-            <button onClick={() => {setIsActive(!isActive)}}>start/stop</button>
+            <button disabled={isLoading} onClick={() => {setIsActive(!isActive)}}>start/stop</button>
         </Container>
 	)
 }
