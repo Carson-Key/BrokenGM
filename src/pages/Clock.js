5b95ae1a7a8bf8e32e1d724ba16725aae14b4017
Carson-Key <keyc@oregonstate.edu>
2022-01-27 21:36:18 -0800

    Add basic timer

--- src/pages/Clock.js
+++ src/pages/Clock.js
@@ -1,10 +1,25 @@
+// Packages
+import { useEffect, useState } from 'react'
 // Components
 import Container from "../components/Container"
 
 const Clock = () => {
+    const [timer, setTimer] = useState(0)
+    const [isActive, setIsActive] = useState(true)
+
+    useEffect(() => {
+        let interval = null
+        if (isActive) {
+          interval = setInterval(() => {
+            setTimer(timer => timer + 1)
+          }, 10)
+        }
+        return () => clearInterval(interval)
+      }, [isActive, timer])
+
     return (
         <Container>
-            Clock
+            {timer}
         </Container>
 	)
 }
