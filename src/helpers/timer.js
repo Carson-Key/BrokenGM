2744dc34777b0bbdf253b25ce37c2f97e52e9448
Carson-Key <keyc@oregonstate.edu>
2022-01-28 19:05:22 -0800

    Add adHour helper

--- src/helpers/timer.js
+++ src/helpers/timer.js
@@ -1,5 +1,16 @@
 import { updateDocument } from './firestore'
 
+export const addHour = (timer, setTimer, timerObject, isClock = false, ammount = 1) => {
+    const amountInMilliseconds = ammount * 3600000
+    setTimer(timer => timer + amountInMilliseconds)
+    
+    updateDocument(
+        "clocks", 
+        "b37722f7-00da-4d7c-b9f5-67325445c313", 
+        { ...timerObject, timer }, 
+        isClock
+    )
+}
 export const addMinute = (timer, setTimer, timerObject, isClock = false, ammount = 1) => {
     const amountInMilliseconds = ammount * 60000
     setTimer(timer => timer + amountInMilliseconds)
@@ -11,7 +22,6 @@ export const addMinute = (timer, setTimer, timerObject, isClock = false, ammount
         isClock
     )
 }
-
 export const addSecond = (timer, setTimer, timerObject, isClock = false, ammount = 1) => {
     const amountInMilliseconds = ammount * 1000
     setTimer(timer => timer + amountInMilliseconds)
@@ -23,7 +33,6 @@ export const addSecond = (timer, setTimer, timerObject, isClock = false, ammount
         isClock
     )
 }
-
 export const addMilliSecond = (timer, setTimer, timerObject, setTimerObject, isClock = false, ammount = 10) => {
     setTimer(timer => timer + ammount)
     if (timer % 60000 === 0) {
@@ -35,4 +44,3 @@ export const addMilliSecond = (timer, setTimer, timerObject, setTimerObject, isC
         )
     }
 }
-
