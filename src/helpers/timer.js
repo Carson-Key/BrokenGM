83e291c30dfa7b4176c7ff68e5760208f0a8b211
Carson-Key <keyc@oregonstate.edu>
2022-01-28 18:37:50 -0800

    Add addMinute helper

--- src/helpers/timer.js
+++ src/helpers/timer.js
@@ -1,5 +1,17 @@
 import { updateDocument } from './firestore'
 
+export const addMinute = (timer, setTimer, timerObject, isClock = false, ammount = 1) => {
+    const amountInMilliseconds = ammount * 60000
+    setTimer(timer => timer + amountInMilliseconds)
+    
+    updateDocument(
+        "clocks", 
+        "b37722f7-00da-4d7c-b9f5-67325445c313", 
+        { ...timerObject, timer }, 
+        isClock
+    )
+}
+
 export const addSecond = (timer, setTimer, timerObject, isClock = false, ammount = 1) => {
     const amountInMilliseconds = ammount * 1000
     setTimer(timer => timer + amountInMilliseconds)
