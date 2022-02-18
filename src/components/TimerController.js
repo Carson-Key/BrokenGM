34616f04a61159e4f988064a0a4567ee8d774f6e
Carson-Key <keyc@oregonstate.edu>
2022-02-18 11:57:37 -0800

    Add onClick event to addUnit helper to TimerController buttons

--- src/components/TimerController.js
+++ src/components/TimerController.js
@@ -1,3 +1,6 @@
+import { TIMETYPES } from "../helpers/objects"
+import { addUnit } from "../helpers/timer"
+
 const TimerController = (props) => {
     const { 
         isLoading, isClock,
@@ -11,12 +14,78 @@ const TimerController = (props) => {
             <button disabled={isLoading} onClick={() => {setIsActive(!isActive)}}>start/stop</button>
             <div>
                 <input type="text" name="Change Clock" placeholder="+/- Time"/>
-                <button disabled={isLoading}>Minutes</button>
-                <button disabled={isLoading}>Hours</button>
-                <button disabled={isLoading}>Days</button>
-                <button disabled={isLoading}>Week</button>
-                <button disabled={isLoading}>Months</button>
-                <button disabled={isLoading}>Years</button>
+                <button 
+                    disabled={isLoading} 
+                    onClick={() => {
+                        addUnit(
+                            timer, setTimer, 
+                            timerObject, setTimerObject, 
+                            TIMETYPES.mins, 1, isClock
+                        )}
+                    }
+                >
+                    Minutes
+                </button>
+                <button
+                    disabled={isLoading} 
+                    onClick={() => {
+                        addUnit(
+                            timer, setTimer, 
+                            timerObject, setTimerObject, 
+                            TIMETYPES.hours, 1, isClock
+                        )}
+                    }
+                >
+                    Hours
+                </button>
+                <button
+                    disabled={isLoading} 
+                    onClick={() => {
+                        addUnit(
+                            timer, setTimer, 
+                            timerObject, setTimerObject, 
+                            TIMETYPES.days, 1, isClock
+                        )}
+                    }
+                >
+                    Days
+                </button>
+                <button
+                    disabled={isLoading} 
+                    onClick={() => {
+                        addUnit(
+                            timer, setTimer, 
+                            timerObject, setTimerObject, 
+                            TIMETYPES.weeks, 1, isClock
+                        )}
+                    }
+                >
+                    Weeks
+                </button>
+                <button
+                    disabled={isLoading} 
+                    onClick={() => {
+                        addUnit(
+                            timer, setTimer, 
+                            timerObject, setTimerObject, 
+                            TIMETYPES.months, 1, isClock
+                        )}
+                    }
+                >
+                    Months
+                </button>
+                <button
+                    disabled={isLoading} 
+                    onClick={() => {
+                        addUnit(
+                            timer, setTimer, 
+                            timerObject, setTimerObject, 
+                            TIMETYPES.years, 1, isClock
+                        )}
+                    }
+                >
+                    Years
+                </button>
             </div>
         </>
 	)
