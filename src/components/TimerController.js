462038f13564d2b9a043ffe1ab59ccde634bf1ab
Carson-Key <keyc@oregonstate.edu>
2022-02-18 11:22:05 -0800

    Add time change ui

--- src/components/TimerController.js
+++ src/components/TimerController.js
@@ -4,6 +4,15 @@ const TimerController = (props) => {
     return (
         <>
             <button disabled={isLoading} onClick={() => {setIsActive(!isActive)}}>start/stop</button>
+            <div>
+                <input type="text" name="Change Clock" placeholder="+/- Time"/>
+                <button>Minutes</button>
+                <button>Hours</button>
+                <button>Days</button>
+                <button>Week</button>
+                <button>Months</button>
+                <button>Years</button>
+            </div>
         </>
 	)
 }
