9dc7ba570d52f5a120112152b96b31781b4fb54d
Carson-Key <keyc@oregonstate.edu>
2022-02-19 12:30:51 -0800

    Add CenterScreen to Loading

--- src/ui/Loading.js
+++ src/ui/Loading.js
@@ -1,14 +1,13 @@
-import Container from "../components/Container"
+// UI
+import CenterScreen from "./CenterScreen"
 
 const Loading = () => {
     return (
-        <Container className="mt-auto">
-            <section className="h-full">
-                <h1 className="text-6xl w-fit mt-auto mx-auto">
-                    Loading...
-                </h1>
-            </section>
-        </Container>
+        <CenterScreen>
+            <h1 className="text-6xl w-fit mt-auto mx-auto">
+                Loading...
+            </h1>
+        </CenterScreen>
     )
 }
 
