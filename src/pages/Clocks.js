51a4eb21d5224e0e38ca14be95dd3ef6c519c9d3
Carson-Key <keyc@oregonstate.edu>
2022-02-19 12:17:48 -0800

    Add getClocks from firbase

--- src/pages/Clocks.js
+++ src/pages/Clocks.js
@@ -11,27 +11,59 @@ import { getCurrentUser } from "../helpers/auth"
 import { getDocument } from "../helpers/firestore"
 
 const Clocks = () => {
-    const [uid, setUID] = useState(0)
+    const [uid, setUID] = useState("")
     const [clocks, setClocks] = useState([])
+    const [clocksArray, setClocksArray] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const [loadedUID, setLoadedUID] = useState(false)
+    const [loadedClocksArray, setLoadedClocksArray] = useState(false)
+    const [loadedClocks, setLoadedClocks] = useState(false)
 
     useEffect(() => {
-        const getClocks = async () => {
-            if (uid) {
-                let clocksData = await (await getDocument("users", uid))
-                setClocks(clocksData.data().clocks)
-                setIsLoading(false)
-            } else {
+        const getClocks = () => {
+            if (!loadedUID) {
                 getCurrentUser(setUID)
+                setLoadedUID(true)
+            }
+            if (loadedUID && !loadedClocksArray) {
+                getDocument("users", uid).then((data) => {
+                    if (data.data().clocks.length === 0) {
+                        setIsLoading(false)
+                    } else {
+                        setClocksArray(data.data().clocks)
+                        setLoadedClocksArray(true)
+                    }
+                })
+            }
+            if (loadedClocksArray && !loadedClocks) {
+                let tempClockArray = []
+                clocksArray.forEach((clock, i) => {
+                    getDocument("clocks", clock).then((data) => {
+                        tempClockArray[i] = {title: data.data().name, id: clock}
+                        if (i === (clocksArray.length - 1)) {
+                            setClocks(tempClockArray)
+                            setLoadedClocks(true)
+                        }
+                    })
+                })
+            }
+            if (loadedUID && loadedClocksArray && loadedClocks) {
+                setIsLoading(false)
             }
         }  
-        getClocks()
-    }, [uid, isLoading])
+        if (isLoading) {
+            getClocks()
+        }
+    })
 
     return (
         <IsLoading isLoading={isLoading}>
             <Container className="flex flex-wrap justify-evenly md:justify-start md:px-2 md:py-1">
-                <ClockCard title="test" />
+                {
+                    clocks.map((clock, i) => {
+                        return <ClockCard key={i} title={clock.title} />
+                    })
+                }
             </Container>
         </IsLoading>
 	)
