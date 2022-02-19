1089462f6cc2c7d7966e732ddbcfb69a29365daa
Carson-Key <keyc@oregonstate.edu>
2022-02-19 09:53:42 -0800

    Add ClockCard

--- src/pages/Clocks.js
+++ src/pages/Clocks.js
@@ -1,10 +1,12 @@
 // Components
 import Container from "../components/Container"
+// UI
+import ClockCard from "../ui/ClockCard"
 
 const Clocks = () => {
     return (
-        <Container>
-
+        <Container className="flex flex-wrap justify-evenly md:justify-start md:px-2 md:py-1">
+            <ClockCard title="test" />
         </Container>
 	)
 }
--- src/ui/ClockCard.js
+++ src/ui/ClockCard.js
@@ -1,6 +1,19 @@
-const ClockCard = () => {
+// Components
+import Card from '../components/Card'
+import CardTitle from '../components/CardTitle'
+
+const ClockCard = (props) => {
+    const { title } = props
+
     return (
-        <></>
+        <Card>
+            <CardTitle>
+                {title}
+            </CardTitle>
+            <div className="transition-all duration-500 ease-out scrollbar-hide overflow-scroll">
+                test
+			</div>
+        </Card>
 	)
 }
 
