7cfe362b90ba2b335169528f1eb3a50309b1f289
Carson-Key <keyc@oregonstate.edu>
2022-02-19 09:38:19 -0800

    Add clocks page

--- src/components/PageRoutes.js
+++ src/components/PageRoutes.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom'
 // Pages
 import Relations from '../pages/Relations'
 import Clock from '../pages/Clock'
+import Clocks from '../pages/Clocks'
 
 const PageRoutes = () => {
     return (
         <Routes>
             <Route path='/login' element={<Relations />} />
             <Route path='/clock/:id' element={<Clock />} />
+            <Route path='/clocks' element={<Clocks />} />
             <Route path='/' element={<Clock />} />
         </Routes>
 	)
---
+++ src/pages/Clocks.js
@@ -0,0 +1,12 @@
+// Components
+import Container from "../components/Container"
+
+const Clocks = () => {
+    return (
+        <Container>
+
+        </Container>
+	)
+}
+
+export default Clocks
