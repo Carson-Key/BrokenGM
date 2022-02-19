10daaf719f8d884ccac2219b1c01682d0023883d
Carson-Key <keyc@oregonstate.edu>
2022-02-19 10:45:50 -0800

    Add IsLoading to Clocks

--- src/pages/Clocks.js
+++ src/pages/Clocks.js
@@ -4,29 +4,34 @@ import { useState } from "react/cjs/react.development"
 import Container from "../components/Container"
 import { getCurrentUser } from "../helpers/auth"
 import { getDocument } from "../helpers/firestore"
+import IsLoading from '../components/IsLoading'
 // UI
 import ClockCard from "../ui/ClockCard"
 
 const Clocks = () => {
     const [uid, setUID] = useState(0)
     const [clocks, setClocks] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         const getClocks = async () => {
             if (uid) {
                 let clocksData = await (await getDocument("users", uid))
                 setClocks(clocksData.data().clocks)
+                setIsLoading(false)
             } else {
                 getCurrentUser(setUID)
             }
         }  
         getClocks()
-    }, [uid])
+    }, [uid, isLoading])
 
     return (
-        <Container className="flex flex-wrap justify-evenly md:justify-start md:px-2 md:py-1">
-            <ClockCard title="test" />
-        </Container>
+        <IsLoading isLoading={isLoading}>
+            <Container className="flex flex-wrap justify-evenly md:justify-start md:px-2 md:py-1">
+                <ClockCard title="test" />
+            </Container>
+        </IsLoading>
 	)
 }
 
