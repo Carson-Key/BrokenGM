b384a364cb21f406ccf43e72ad0aac7db8b91e91
Carson-Key <keyc@oregonstate.edu>
2022-02-19 10:26:35 -0800

    Add Container to Loading

--- src/ui/Loading.js
+++ src/ui/Loading.js
@@ -1,10 +1,14 @@
+import Container from "../components/Container"
+
 const Loading = () => {
     return (
-        <section className="h-full">
-            <h1 className="text-6xl w-fit mt-auto mx-auto">
-                Loading...
-            </h1>
-        </section>
+        <Container className="mt-auto">
+            <section className="h-full">
+                <h1 className="text-6xl w-fit mt-auto mx-auto">
+                    Loading...
+                </h1>
+            </section>
+        </Container>
     )
 }
 
