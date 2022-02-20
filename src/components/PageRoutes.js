2c8ce7727604971f4db2b75a38cfea3533a723c9
Carson-Key <keyc@oregonstate.edu>
2022-02-20 15:09:23 -0800

    Remove test code

--- src/components/PageRoutes.js
+++ src/components/PageRoutes.js
@@ -1,26 +1,12 @@
 // Packages
 import { Routes, Route } from 'react-router-dom'
-import { useContext, useEffect } from 'react'
 // Pages
 import Relations from '../pages/Relations'
 import Login from '../pages/Login'
 import Clock from '../pages/Clock'
 import Clocks from '../pages/Clocks'
-// Contexts
-import { NotificationContext } from '../contexts/Notification'
 
 const PageRoutes = () => {
-    const setNotification = useContext(NotificationContext)[1]
-
-    useEffect(() => {
-        setNotification(
-            {
-                type: 'SET_NOTIFICATION', 
-                payload: {occurs: true, message: "Hello", type: "error"}
-            }
-        )
-    }, [])
-
     return (
         <Routes>
             <Route path='/login' element={<Login />} />
