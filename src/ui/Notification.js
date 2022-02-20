2472d420b770bc8614ed0f1ba789142312cf2759
Carson-Key <keyc@oregonstate.edu>
2022-02-20 15:24:00 -0800

    Add padding x to notification

--- src/ui/Notification.js
+++ src/ui/Notification.js
@@ -2,7 +2,7 @@ const Notification = (props) => {
     const { message, className } = props
 
     return (
-        <div className="absolute top-14 w-screen">
+        <div className="absolute top-14 w-screen px-1">
             <div className={"w-fit rounded-lg text-xl px-5 py-2 mt-1 mx-auto " + className}>
                 <p>{message}</p>
             </div>
