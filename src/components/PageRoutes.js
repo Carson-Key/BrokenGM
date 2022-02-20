64a7e591fef5b2b402ff3f6ba2418bbc83fcf877
Carson-Key <keyc@oregonstate.edu>
2022-02-20 10:24:43 -0800

    Add Login page

--- src/components/PageRoutes.js
+++ src/components/PageRoutes.js
@@ -2,13 +2,14 @@
 import { Routes, Route } from 'react-router-dom'
 // Pages
 import Relations from '../pages/Relations'
+import Login from '../pages/Login'
 import Clock from '../pages/Clock'
 import Clocks from '../pages/Clocks'
 
 const PageRoutes = () => {
     return (
         <Routes>
-            <Route path='/login' element={<Relations />} />
+            <Route path='/login' element={<Login />} />
             <Route path='/clock/:id' element={<Clock />} />
             <Route path='/clocks' element={<Clocks />} />
             <Route path='/' element={<Clock />} />
---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+// Components
+import CenterScreen from "../ui/CenterScreen"
+
+const Login = () => {
+    return (
+        <CenterScreen>
+            <h1 className="text-6xl w-screen text-center mt-auto mx-auto">
+                Please login with the button in the top right
+            </h1>
+        </CenterScreen>
+	)
+}
+
+export default Login
