53441d0034d6c080092e32640dfbeb1791f38e00
Carson-Key <keyc@oregonstate.edu>
2022-02-26 12:01:18 -0800

    Add call to relations

--- src/pages/Relation.js
+++ src/pages/Relation.js
@@ -1,15 +1,59 @@
 // Packages
+import { useEffect, useState, useContext } from 'react'
 import { useParams } from 'react-router-dom'
+// Components
+import Timer from "../components/Timer"
+import TimerController from "../components/TimerController"
+import IsLoading from '../components/IsLoading'
+import ConditionalRender from '../components/ConditionalRender'
 // UI
 import Container from "../ui/Container"
+// Contexts
+import { NotificationContext } from "../contexts/Notification"
+// Helpers
+import { getDocument } from "../helpers/firestore"
+import { getCurrentUser } from '../helpers/auth'
 
 const Relation = () => {
     const { id } = useParams()
-    
+    const setNotification = useContext(NotificationContext)[1]
+    const [isLoading, setIsLoading] = useState(true)
+    const [relations, setRelations] = useState([])
+    const [isRelation, setIsRelation] = useState(false)
+    const [isAdmin, setIsAdmin] = useState(false)
+    const [uid, setUID] = useState("")
+
+    useEffect(() => {
+        const getClockData = async () => {
+            getDocument("relations", id, setNotification).then((data) => {
+                setRelations(data.data().npcs)
+                setIsRelation(data.exists())
+                getCurrentUser(setUID, (uid) => {
+                    if (data.data().admins.includes(uid)) {
+                        setIsAdmin(true)
+                    }
+                })
+                setIsLoading(false)
+            })
+        }
+
+        if (isLoading) {
+            getClockData()
+        }
+    }, [id, isLoading, uid, setNotification])
+
     return (
-        <Container className="flex flex-wrap justify-evenly md:justify-start md:px-2 md:py-1">
-            {id}
-        </Container>
+        <IsLoading isLoading={isLoading}>
+            <Container className="mt-auto">
+                {
+                    relations.map((relation) => {
+                        return (
+                            <p>{relation.name}</p>
+                        )
+                    })
+                }
+            </Container>
+        </IsLoading>
 	)
 }
 
