43b7dd9011fe31f6ae09f08ae8f955cfe87bb061
Carson-Key <keyc@oregonstate.edu>
2022-03-02 16:26:23 -0800

    Fix larger screen size css for relation controller

--- src/components/CharacterRelationController.js
+++ src/components/CharacterRelationController.js
@@ -2,8 +2,8 @@ const CharacterRelationController = (props) => {
     const { character, relation } = props
 
     return (
-        <div className="mt-2 mx-auto w-fit text-center flex text-md md:text-3xl">
-            <div className="border-y border-l rounded-l-md px-1 py-1 md:px-3 md:py-2 divide divide-x">
+        <div className="mt-2 mx-auto w-fit text-center flex text-md">
+            <div className="border-y border-l rounded-l-md px-1 py-1 divide divide-x">
                 <button 
                     className="px-2"
                 >
@@ -11,12 +11,12 @@ const CharacterRelationController = (props) => {
                 </button>
             </div>
             <input 
-                className="bg-gray-100 text-center px-1 py-1 md:px-3 w-10 md:w-40"
+                className="bg-gray-100 text-center px-1 py-1 w-12"
                 type="text" 
                 name="Change Clock" 
                 placeholder={relation[character]}
             />
-            <div className="border-y border-r rounded-r-md px-1 py-1 md:px-3 md:py-2 divide divide-x">
+            <div className="border-y border-r rounded-r-md px-1 py-1 divide divide-x">
                 <button 
                     className="px-2"
                 >
