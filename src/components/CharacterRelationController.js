3f39066b853d91554c0745a1ce153524273037f8
Carson-Key <keyc@oregonstate.edu>
2022-03-02 16:23:16 -0800

    Extract relation controller to own component

---
+++ src/components/CharacterRelationController.js
@@ -0,0 +1,30 @@
+const CharacterRelationController = (props) => {
+    const { character, relation } = props
+
+    return (
+        <div className="mt-2 mx-auto w-fit text-center flex text-md md:text-3xl">
+            <div className="border-y border-l rounded-l-md px-1 py-1 md:px-3 md:py-2 divide divide-x">
+                <button 
+                    className="px-2"
+                >
+                    -
+                </button>
+            </div>
+            <input 
+                className="bg-gray-100 text-center px-1 py-1 md:px-3 w-10 md:w-40"
+                type="text" 
+                name="Change Clock" 
+                placeholder={relation[character]}
+            />
+            <div className="border-y border-r rounded-r-md px-1 py-1 md:px-3 md:py-2 divide divide-x">
+                <button 
+                    className="px-2"
+                >
+                    +
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default CharacterRelationController
--- src/components/RelationOfCharacters.js
+++ src/components/RelationOfCharacters.js
@@ -1,4 +1,6 @@
-// Helepr
+// Components
+import CharacterRelationController from './CharacterRelationController'
+// Helpers
 import { formatCharacterName } from '../helpers/relation'
 import { RELATIONINDICATORCLASSES } from '../helpers/objects'
 import { defaultAccessArray } from '../helpers/misc'
@@ -23,30 +25,10 @@ const RelationOfCharacter = (props) => {
                             }
                         ></div>
                     </div>
-                    <div className="mt-2 mx-auto w-fit text-center flex text-md md:text-3xl">
-                        <div className="border-y border-l rounded-l-md px-1 py-1 md:px-3 md:py-2 divide divide-x">
-                            <button 
-                                key={i}
-                                className="px-2"
-                            >
-                                -
-                            </button>
-                        </div>
-                        <input 
-                            className="bg-gray-100 text-center px-1 py-1 md:px-3 w-10 md:w-40"
-                            type="text" 
-                            name="Change Clock" 
-                            placeholder={relation[character]}
-                        />
-                        <div className="border-y border-r rounded-r-md px-1 py-1 md:px-3 md:py-2 divide divide-x">
-                            <button 
-                                key={i}
-                                className="px-2"
-                            >
-                                +
-                            </button>
-                        </div>
-                    </div>
+                    <CharacterRelationController 
+                        character={character}
+                        relation={relation}
+                    />
                 </div>
             )
         })
