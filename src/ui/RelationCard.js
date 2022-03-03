03a19064436ba639f98ebd6d4e158f0cec698301
Carson-Key <keyc@oregonstate.edu>
2022-03-02 16:13:22 -0800

    Add button/textarea to relations

--- src/ui/RelationCard.js
+++ src/ui/RelationCard.js
@@ -11,18 +11,18 @@ const RelationCard = (props) => {
     const relationKeys = Object.keys(relation).sort()
 
     return (
-        <Card className="h-80 overflow-scroll">
+        <Card className="h-80">
             <CardTitle>
                 {name}
             </CardTitle>
-            <div className="my-3 mx-4 w-72 divide-y">
+            <div className="my-3 mx-4 w-72 overflow-scroll h-64 divide-y">
                 {
                     relationKeys.map((character, i) => {
                         return (
                             <div className="my-3">
                                 <p key={i}>{formatCharacterName(character)}</p>
                                 <div 
-                                    className="rounded-lg h-3 w-full bg-gradient-to-r from-red-500 to-green-500"
+                                    className="rounded-lg h-fit w-full bg-gradient-to-r from-red-500 to-green-500"
                                 >
                                     <div 
                                         className={
@@ -32,6 +32,30 @@ const RelationCard = (props) => {
                                             )
                                         }
                                     ></div>
+                                </div>
+                                <div className="mt-2 mx-auto w-fit text-center flex text-md md:text-3xl">
+                                    <div className="border-y border-l rounded-l-md px-1 py-1 md:px-3 md:py-2 divide divide-x">
+                                        <button 
+                                            key={i}
+                                            className="px-2"
+                                        >
+                                            -
+                                        </button>
+                                    </div>
+                                    <input 
+                                        className="bg-gray-100 text-center px-1 py-1 md:px-3 w-10 md:w-40"
+                                        type="text" 
+                                        name="Change Clock" 
+                                        placeholder={relation[character]}
+                                    />
+                                    <div className="border-y border-r rounded-r-md px-1 py-1 md:px-3 md:py-2 divide divide-x">
+                                        <button 
+                                            key={i}
+                                            className="px-2"
+                                        >
+                                            +
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         )
