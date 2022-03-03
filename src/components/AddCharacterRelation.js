8f60806e615083dcdcd4bf794d6dbb0e4ba8d1e0
Carson-Key <keyc@oregonstate.edu>
2022-03-03 13:53:35 -0800

    Add state managment for text field

--- src/components/AddCharacterRelation.js
+++ src/components/AddCharacterRelation.js
@@ -1,7 +1,10 @@
 // Packages
 import { GrAddCircle } from 'react-icons/gr'
+import { useState } from 'react'
 
 const AddCharacterRelation = (props) => {
+    const [nameToAdd, setNameToAdd] = useState("")
+
     return (
         <div className="w-full">
             <div className="py-2 text-2xl mx-auto w-fit h-fit">
@@ -9,6 +12,10 @@ const AddCharacterRelation = (props) => {
                     className="border rounded-lg border-slate-400 text-center h-9 px-1 py-1 w-40 inline"
                     type="text" 
                     name="Add Character"
+                    placeholder="John Doe"
+                    onChange={(event) => {
+                        setNameToAdd(event.target.value)
+                    }}
                 />
                 <GrAddCircle className="inline h-9 mx-1 mb-1" />
             </div>
