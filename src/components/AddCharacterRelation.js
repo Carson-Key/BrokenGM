43bde579968d1cc3f2113fec3c53fb1ab596d6b2
Carson-Key <keyc@oregonstate.edu>
2022-03-03 13:45:36 -0800

    Add input and plus button to AddCharacterRelation

--- src/components/AddCharacterRelation.js
+++ src/components/AddCharacterRelation.js
@@ -1,7 +1,16 @@
+import { GrAddCircle } from "react-icons/gr";
+
 const AddCharacterRelation = (props) => {
     return (
-        <div className="py-2 text-center">
-            <button>Add New Character</button>
+        <div className="w-full">
+            <div className="py-2 text-2xl mx-auto w-fit h-fit">
+                <input 
+                    className="border rounded-lg border-slate-400 text-center h-9 px-1 py-1 w-40 inline"
+                    type="text" 
+                    name="Add Character"
+                />
+                <GrAddCircle className="inline h-9 mx-1 mb-1" />
+            </div>
         </div>
     )
 }
