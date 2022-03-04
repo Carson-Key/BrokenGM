41312d9665a71515f27aa5bf951d91a0ba656a14
Carson-Key <keyc@oregonstate.edu>
2022-03-04 11:43:16 -0800

    Add nagivate to login if user is not logged in

--- src/components/PageRoutes.js
+++ src/components/PageRoutes.js
@@ -6,17 +6,22 @@ import Relation from '../pages/Relation'
 import Login from '../pages/Login'
 import Clock from '../pages/Clock'
 import Clocks from '../pages/Clocks'
+// Component
+import RerouteToLogin from './RerouteToLogin'
 
 const PageRoutes = () => {
     return (
-        <Routes>
-            <Route path='/login' element={<Login />} />
-            <Route path='/clock/:id' element={<Clock />} />
-            <Route path='/clocks' element={<Clocks />} />
-            <Route path='/relation/:id' element={<Relation />} />
-            <Route path='/relations' element={<Relations />} />
-            <Route path='/' element={<Clocks />} />
-        </Routes>
+        <>
+            <Routes>
+                <Route path='/login' element={<Login />} />
+                <Route path='/clock/:id' element={<Clock />} />
+                <Route path='/clocks' element={<Clocks />} />
+                <Route path='/relation/:id' element={<Relation />} />
+                <Route path='/relations' element={<Relations />} />
+                <Route path='/' element={<Clocks />} />
+            </Routes>
+            <RerouteToLogin />
+        </>
 	)
 }
 
