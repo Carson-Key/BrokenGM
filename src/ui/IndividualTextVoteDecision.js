5bba24f6ff02c0a5ba09a5ef6fedf3b078576dab
Carson-Key <keyc@oregonstate.edu>
2022-05-05 15:43:46 -0700

    Add prop to edit colors

--- src/ui/IndividualTextVoteDecision.js
+++ src/ui/IndividualTextVoteDecision.js
@@ -2,20 +2,26 @@
 import { useState, useEffect } from "react"
 
 const IndividualTextVoteDecision = (props) => {
-    const { children } = props
+    const { children, colors } = props
     const [className, setClasseName] = useState("")
+    const colorObject = useState(colors ? colors : {
+        yes: "text-white bg-green-400",
+        no: "text-white bg-red-400",
+        none: "text-white bg-gray-400",
+        default: "text-white bg-sky-400"
+    })[0]
 
     useEffect(() => {
         if (children.toLowerCase() === "yes") {
-            setClasseName("text-white bg-green-400")
+            setClasseName(colorObject.yes)
         } else if (children.toLowerCase() === "no") {
-            setClasseName("text-white bg-red-400")
+            setClasseName(colorObject.no)
         } else if (children.toLowerCase() === "abstain" || children.toLowerCase() === "") {
-            setClasseName("text-white bg-gray-400")
+            setClasseName(colorObject.none)
         } else {
-            setClasseName("text-white bg-sky-400")
+            setClasseName(colorObject.default)
         }
-    }, [children])
+    }, [children, colorObject])
 
     return (
         <p className={"w-fit mx-auto py-1 px-3 rounded-xl " + className}>{children}</p>
