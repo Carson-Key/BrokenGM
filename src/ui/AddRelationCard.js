c247da6a2e8d93a173f1a2fa6db02e342efb0fc4
Carson-Key <keyc@oregonstate.edu>
2022-05-22 11:34:26 -0700

    Add scrollbar hide to add relation card

--- src/ui/AddRelationCard.js
+++ src/ui/AddRelationCard.js
@@ -18,7 +18,7 @@ const RelationCard = (props) => {
             <CardTitle>
                 Add New Relation
             </CardTitle>
-            <div className="text-3xl my-3 mx-4 w-72 overflow-scroll h-64 divide-y">
+            <div className="text-3xl my-3 mx-4 w-72 overflow-scroll scrollbar-hide h-64 divide-y">
                 <div className="flex flex-col items-center justify-center mx-auto w-fit mt-auto h-full ">
                     <input 
                             className="border rounded-lg border-slate-400 text-center h-9 px-2 py-2 w-48"
