fe4ff1b9c118dfea2233e6a4f2fb1d03528cbb3d
Carson-Key <keyc@oregonstate.edu>
2022-05-22 11:30:55 -0700

    Add css to bette fit screen for grid layout

--- src/ui/VoteSystemGrid.js
+++ src/ui/VoteSystemGrid.js
@@ -1,22 +1,26 @@
 // UI
-import Vote from '../ui/Vote'
+import VoteGrid from '../ui/VoteGrid'
 
 const VoteSystemGrid = (props) => {
     const { votes } = props
 
     return (
-        votes.map((vote, i) => {
-            if (vote.locked  === true) {
-                return (
-                    <Vote 
-                        votes={votes}
-                        currentVote={i}
-                    />
-                )
-            } else {
-                return <></>
+        <section className="flex flex-wrap justify-center">
+            {
+                votes.map((vote, i) => {
+                    if (vote.locked  === true) {
+                        return (
+                            <VoteGrid
+                                votes={votes}
+                                currentVote={i}
+                            />
+                        )
+                    } else {
+                        return <></>
+                    }
+                })
             }
-        })
+        </section>
 	)
 }
 
