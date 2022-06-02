a5a613ce44035c415d6c7b3839017e1f58f824ec
Carson-Key <keyc@oregonstate.edu>
2022-06-02 15:56:33 -0700

    Add className prop

--- src/components/VotingSystem/IndividualArchivedVote.js
+++ src/components/VotingSystem/IndividualArchivedVote.js
@@ -1,3 +1,5 @@
+// Packages
+import { useState } from "react"
 // VotingSystem
 import IndividualArchivedVoteDecision from "./IndividualTextVoteDecision"
 // Helpers
@@ -5,10 +7,11 @@ import { returnChildOfObject } from '../../helpers/misc'
 import { formatCharacterName } from '../../helpers/voting'
 
 const IndividualArchivedVote = (props) => {
-    const { votes, currentVote, vote } = props
+    const { votes, currentVote, vote, className } = props
+    const classNameState = useState(className ? "" + className : "")[0]
 
     return (
-        <div className="text-center py-2 px-4 mx-2">
+        <div className={"text-center py-2 px-4 mx-2" + classNameState}>
             <h3 className="font-bold">
                 {formatCharacterName(vote)}
             </h3>
