aa82c0338c448d12fe6525c0ecee2b2bbee4a5ff
Carson-Key <keyc@oregonstate.edu>
2022-06-03 15:18:38 -0700

    Add name call from DB for card title

--- src/components/CampaignLinkCard.js
+++ src/components/CampaignLinkCard.js
@@ -1,17 +1,42 @@
+// Packages
+import { useContext, useState } from 'react'
+// Components
+import ConditionalRender from './ConditionalRender'
 // UI
 import Card from '../ui/Card'
 import CardTitle from '../ui/CardTitle'
+// Contexts
+import { NotificationContext } from "../contexts/Notification"
+// Helpers
+import { getDocument } from '../helpers/firestore'
 
 const CampaignLinkCard = (props) => {
-    const { items, isAdmin, uid, players } = props
+    const { items, isAdmin, uid, players, docID } = props
+    const setNotification = useContext(NotificationContext)[1]
+    const [itemObject, setItemObject] = useState({name: "Loading..."})
 
     return (
         items.map((item, i) => {
+            getDocument(docID, item, setNotification).then((data) => {
+                const itemData = data.data()
+                setItemObject({name: itemData.name})
+            })
+
             return (
-                <Card key={i}>
-                    <CardTitle>Test</CardTitle>
-                    <p>This is Item: {item}</p>
-                </Card>
+                <ConditionalRender
+                    condition={isAdmin}
+                    returnComponent={
+                        <Card key={i}>
+                            <CardTitle>{itemObject.name}</CardTitle>
+                            <p>This is Item: {item}</p>
+                        </Card>
+                    }
+                >
+                    <Card key={i}>
+                        <CardTitle>{itemObject.name}</CardTitle>
+                        <p>This is Item: {item}</p>
+                    </Card>
+                </ConditionalRender>
             )
         })
     )
