4dc361ecfeda90d77319e96834051c2eebfb0a1a
Carson-Key <keyc@oregonstate.edu>
2022-06-02 21:46:05 -0700

    Add mario's highly requested change

--- src/pages/Relations.js
+++ src/pages/Relations.js
@@ -4,7 +4,7 @@ import PageOfLinkCards from '../components/PageOfLinkCards'
 const Relations = () => {
     return (
         <PageOfLinkCards 
-            noCardText="You Don't Have Any Relations"
+            noCardText="You Don't Have Any Maidens"
             cardInnerText="To Relation page"
             docID="relations"
             toPath="relation"
