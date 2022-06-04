bc81790cbafdac428b0c6e9bf44f8f371bb3c6f2
Carson-Key <keyc@oregonstate.edu>
2022-06-04 10:56:34 -0700

    Add player settings to Voting Systems

--- src/components/Campaign/VotingSystemSettings.js
+++ src/components/Campaign/VotingSystemSettings.js
@@ -1,7 +1,56 @@
-const VotingSystemSettings = () => {
+// Packages
+import { useState, useEffect, useContext } from "react"
+// Campaign
+import EditPlayers from "./EditPlayers"
+// Contexts
+import { NotificationContext } from "../../contexts/Notification"
+// Helpers
+import { getDocument, updateDocument } from "../../helpers/firestore"
+import { returnChildOfObject, removeElementFromArray } from "../../helpers/misc"
+
+const ClockSettings = (props) => {
+    const { players, id } = props
+    const setNotification = useContext(NotificationContext)[1]
+    const [votingSystemPlayers, setVotingSystemPlayers] = useState({...players})
+    const [isVotingSystem, setIsVotingSystem] = useState(false)
+    const [activePlayers, setActivePlayers] = useState([])
+
+    useEffect(() => {
+        getDocument("votingsystems", id, setNotification).then((data)  => {
+            const clockPlayersDB = data.data().players
+            setActivePlayers(clockPlayersDB)
+            setIsVotingSystem(data.exists())
+            let tempEnabledPlayers = {...players}
+            clockPlayersDB.forEach((player) => {    
+                tempEnabledPlayers[player] = {id: player, name: returnChildOfObject(players, [player, "name"], ''), access: true}
+            })
+            
+            setVotingSystemPlayers(tempEnabledPlayers)
+        })
+    }, [players, id, setNotification])
+
     return (
-        <p>You Are the Admin</p>
+        <EditPlayers
+            players={votingSystemPlayers}
+            toggleAccess={(event, player) => {
+                const playerObject = votingSystemPlayers[player]
+                if (playerObject.access) {
+                    let tempActivePlayers = [...activePlayers]
+                    tempActivePlayers = removeElementFromArray(tempActivePlayers, player)
+                    setActivePlayers(tempActivePlayers)
+                    updateDocument("votingsystems", id, {players: tempActivePlayers}, setNotification, isVotingSystem)
+                } else {
+                    let tempActivePlayers = [...activePlayers, player]
+                    setActivePlayers(tempActivePlayers)
+                    updateDocument("votingsystems", id, {players: tempActivePlayers}, setNotification, isVotingSystem)
+                }
+                setVotingSystemPlayers({
+                    ...votingSystemPlayers, 
+                    [player]: {...playerObject, access: !playerObject.access}
+                })
+            }}
+        />
     )
 }
 
-export default VotingSystemSettings
+export default ClockSettings
