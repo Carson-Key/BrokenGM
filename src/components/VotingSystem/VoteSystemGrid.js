930d42aa52822dfa661ab40e76fc668fd2cdaf7e
Carson-Key <keyc@oregonstate.edu>
2022-06-04 15:40:11 -0700

    Add display message if there are no past votes

--- src/components/VotingSystem/VoteSystemGrid.js
+++ src/components/VotingSystem/VoteSystemGrid.js
@@ -1,16 +1,18 @@
 // Packages
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 // VotingSystem
 import VoteGrid from './VoteGrid'
 
 const VoteSystemGrid = (props) => {
     const { votes } = props
+    let votesDisplayed = 0
 
     return (
         <section className="flex flex-wrap justify-center">
             {
                 votes.map((vote, i) => {
                     if (vote.locked  === true) {
+                        votesDisplayed = votesDisplayed + 1
                         return (
                             <VoteGrid
                                 key={i}
@@ -18,7 +20,16 @@ const VoteSystemGrid = (props) => {
                                 currentVote={i}
                             />
                         )
-                    } else {
+                    } else if ((i === votes.length - 1) && (votesDisplayed === 0)) {
+                        return (
+                            <h2 
+                                key={i} 
+                                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-4xl">
+                                    There are no Past Votes
+                            </h2>
+                        )
+                    }
+                    else {
                         return <Fragment key={i}></Fragment>
                     }
                 })
