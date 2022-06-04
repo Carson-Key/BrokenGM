dcfc58b16e804a5a0cc5d53454312097ce920a01
Carson-Key <keyc@oregonstate.edu>
2022-06-04 10:49:59 -0700

    Add functionality to remove/add access to campaign item

--- src/components/Campaign/ClockSettings.js
+++ src/components/Campaign/ClockSettings.js
@@ -5,17 +5,21 @@ import EditPlayers from "./EditPlayers"
 // Contexts
 import { NotificationContext } from "../../contexts/Notification"
 // Helpers
-import { getDocument } from "../../helpers/firestore"
-import { returnChildOfObject } from "../../helpers/misc"
+import { getDocument, updateDocument } from "../../helpers/firestore"
+import { returnChildOfObject, removeElementFromArray } from "../../helpers/misc"
 
 const ClockSettings = (props) => {
     const { players, id } = props
     const setNotification = useContext(NotificationContext)[1]
     const [clockPlayers, setClockPlayers] = useState({...players})
+    const [isClocks, setIsClock] = useState(false)
+    const [activePlayers, setActivePlayers] = useState([])
 
     useEffect(() => {
         getDocument("clocks", id, setNotification).then((data)  => {
             const clockPlayersDB = data.data().players
+            setActivePlayers(clockPlayersDB)
+            setIsClock(data.exists())
             let tempEnabledPlayers = {...players}
             clockPlayersDB.forEach((player) => {    
                 tempEnabledPlayers[player] = {id: player, name: returnChildOfObject(players, [player, "name"], ''), access: true}
@@ -29,7 +33,21 @@ const ClockSettings = (props) => {
         <EditPlayers
             players={clockPlayers}
             toggleAccess={(event, player) => {
-
+                const playerObject = clockPlayers[player]
+                if (playerObject.access) {
+                    let tempActivePlayers = [...activePlayers]
+                    tempActivePlayers = removeElementFromArray(tempActivePlayers, player)
+                    setActivePlayers(tempActivePlayers)
+                    updateDocument("clocks", id, {players: tempActivePlayers}, setNotification, isClocks)
+                } else {
+                    let tempActivePlayers = [...activePlayers, player]
+                    setActivePlayers(tempActivePlayers)
+                    updateDocument("clocks", id, {players: tempActivePlayers}, setNotification, isClocks)
+                }
+                setClockPlayers({
+                    ...clockPlayers, 
+                    [player]: {...playerObject, access: !playerObject.access}
+                })
             }}
         />
     )
