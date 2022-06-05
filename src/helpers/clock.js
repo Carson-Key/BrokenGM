b7f46b564f9472e0a5a863e25016c0462e36a8be
Carson-Key <keyc@oregonstate.edu>
2022-06-05 15:37:18 -0700

    Add logic when timer overflows

--- src/helpers/clock.js
+++ src/helpers/clock.js
@@ -4,4 +4,30 @@ export const tickTimer = (
     isAdmin, isClock 
 ) => {
     setTimer(prev => prev + 10)
+    if (timer >= (3600000 * clock.hoursInDay)) {
+        checkForOverflow(timer, clock, setTimer, setClock)
+    }
+}
+
+const checkForOverflow = (timer, clock, setTimer, setClock) => {
+    const timerExcess = timer - (3600000 * clock.hoursInDay)
+    const tempClock = {...clock}
+    if (timerExcess >= 0) {
+        tempClock.dayOfMonth = tempClock.dayOfMonth + 1
+        tempClock.dayOfWeek = tempClock.dayOfWeek + 1
+        if (tempClock.dayOfMonth > tempClock.daysInMonths[tempClock.monthOfYear]) {
+            tempClock.monthOfYear = tempClock.monthOfYear + 1
+            tempClock.dayOfMonth = 1
+            if (tempClock.monthOfYear === tempClock.daysInMonths.length) {
+                tempClock.monthOfYear = 0
+                tempClock.year = tempClock.year + 1
+            }
+        }
+        if (tempClock.dayOfWeek === tempClock.daysOfWeek.length) {
+            tempClock.dayOfWeek = 0
+        }
+        tempClock.timer = timerExcess
+        setTimer(timerExcess)
+        setClock(tempClock)
+    }
 }
