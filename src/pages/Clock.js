08b68c3ac7c54e7de1096e648c239d751634eecb
Carson-Key <keyc@oregonstate.edu>
2022-06-05 11:35:41 -0700

    Add call to db to get clock object

--- src/pages/Clock.js
+++ src/pages/Clock.js
@@ -1,4 +1,37 @@
+// Packages
+import { useEffect, useState, useContext } from 'react'
+import { useParams } from 'react-router-dom'
+// Contexts
+import { NotificationContext } from "../contexts/Notification"
+// Helpers
+import { getDocument } from "../helpers/firestore"
+import { getCurrentUser } from '../helpers/auth'
+
 const Clock = () => {
+    const { id } = useParams()
+    const setNotification = useContext(NotificationContext)[1]
+    const [isLoading, setIsLoading] = useState(true)
+    const [timer, setTimer] = useState(0)
+    const [clock, setClock] = useState({})
+    const [isClock, setIsClock] = useState(false)
+    const [isAdmin, setIsAdmin] = useState(false)
+    const [uid, setUID] = useState("")
+
+    useEffect(() => {
+        if (isLoading) {
+            getDocument("clocks", id, setNotification).then((data) => {
+                setTimer(data.data().timer)
+                setClock(data.data())
+                setIsClock(data.exists())
+                getCurrentUser(setUID, (uid) => {
+                    if (data.data().admins.includes(uid)) {
+                        setIsAdmin(true)
+                    }
+                })
+                setIsLoading(false)
+            })
+        }
+    }, [id, isLoading, uid, setNotification, clock, timer])
 
     return (
         <p>Clock</p>
