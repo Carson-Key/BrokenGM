bf03f657e54ea24556dede01271a72a3a595fb0b
Carson-Key <keyc@oregonstate.edu>
2022-06-09 20:54:20 -0700

    Add navigation ui for events

--- src/pages/ClockEvents.js
+++ src/pages/ClockEvents.js
@@ -1,5 +1,5 @@
 // Packages
-import { useEffect, useState, useContext } from 'react'
+import { Fragment, useEffect, useState, useContext } from 'react'
 import { useParams } from 'react-router-dom'
 // Components
 import IsLoading from '../components/IsLoading'
@@ -19,6 +19,7 @@ const Relation = () => {
     const setNotification = useContext(NotificationContext)[1]
     const [isLoading, setIsLoading] = useState(true)
     const [events, setEvents] = useState([])
+    const [eventsDisplayStyle, setEventsDisplayStyle] = useState("new")
     const [clockData, setClockData] = useState(null)
     const [isClockEvents, setIsClockEvents] = useState(false)
     const [isAdmin, setIsAdmin] = useState(false)
@@ -52,11 +53,35 @@ const Relation = () => {
 
     return (
         <IsLoading isLoading={isLoading}>
+            <div className="my-4 divide-x mx-auto">
+                <button className="rounded-l-lg bg-gray-100 px-3 py-2" onClick={() => {
+                    setEventsDisplayStyle("new")
+                }}>
+                    <h3>Coming Events</h3>
+                </button>
+                <button className="rounded-r-lg bg-gray-100 px-3 py-2" onClick={() => {
+                    setEventsDisplayStyle("old")
+                }}>
+                    <h3>Past Events</h3>
+                </button>
+            </div>
+            <button className="w-fit mx-auto text-white bg-green-500 rounded-md px-2 py-1">
+                Add New Event
+            </button>
             <Container className="flex flex-wrap justify-evenly md:px-2 md:py-1 mx-auto">
                 {
                     events.map((event, i) => {
                         const { time, description } = parseEventString(event)
 
+                        if (clockData) {
+                            if (
+                                (time.year >= clockData.year && 
+                                time.month >= clockData.monthOfyear &&
+                                time.day >= clockData.dayOfMonth) && time.timer > clockData.timer
+                            ) {
+                                return <Fragment key={i}></Fragment>
+                            } 
+                        }
                         return (
                             <EventCard 
                                 key={i}
