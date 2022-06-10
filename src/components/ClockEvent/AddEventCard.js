e822e1fd4f599217d4a23d9b50db97eb1ebef30d
Carson-Key <keyc@oregonstate.edu>
2022-06-09 23:09:02 -0700

    Add input validation to new event

--- src/components/ClockEvent/AddEventCard.js
+++ src/components/ClockEvent/AddEventCard.js
@@ -3,6 +3,8 @@ import { useState, useContext } from 'react'
 // UI
 import Card from '../../ui/Card'
 import CardTitle from '../../ui/CardTitle'
+// Helpers
+import { saveEvent, checkInput } from '../../helpers/clockevents'
 // Contexts
 import { NotificationContext } from '../../contexts/Notification'
 
@@ -20,10 +22,11 @@ const AddEventCard = (props) => {
             <div className="flex flex-col my-2 mx-4 w-72 scrollbar-hide overflow-scroll">
                 <div>
                     <p>Time Stamp</p>
+                    <p>HH:HH(24 hour)/DD/MM/YYYY</p>
                     <input 
                         className="border rounded-lg border-slate-400 h-9 px-2 py-2 w-full"
                         type="text" 
-                        name="Add Character"
+                        name="Add Time Stamp"
                         placeholder="HH:HH(24 hour)/DD/MM/YYYY"
                         value={timeToAdd}
                         onChange={(event) => {
@@ -34,7 +37,7 @@ const AddEventCard = (props) => {
                 <div>
                     <p>Description</p>
                     <textarea 
-                        className="border rounded-lg text-left border-slate-400 break-words px-2 py-2 w-full h-28"
+                        className="border rounded-lg text-left border-slate-400 break-words px-2 py-2 w-full h-24"
                         type="text-area" 
                         name="Add Description"
                         placeholder="Event description..."
@@ -48,7 +51,30 @@ const AddEventCard = (props) => {
                     className="my-3 mx-2 text-base rounded-lg border bg-green-500 text-white px-2 py-1"
                     onClick={
                         () => {
-                            
+                            const splitTime = timeToAdd.split("/")
+                            const splitHoursMinis = splitTime[0].split(":")
+                            const time = {
+                                minsNumber: parseInt(splitHoursMinis[1]),
+                                hoursNumber: parseInt(splitHoursMinis[0]),
+                                daysNumber: parseInt(splitTime[1]),
+                                monthsNumber: parseInt(splitTime[2]),
+                                yearsNumber: parseInt(splitTime[3]),
+                                minsString: splitHoursMinis[1],
+                                hoursString: splitHoursMinis[0],
+                                daysString: splitTime[1],
+                                monthsString: splitTime[2],
+                                yearsString: splitTime[3],
+                            }
+                            if (checkInput(
+                                [time.minsNumber, time.hoursNumber, time.daysNumber, time.monthsNumber, time.yearsNumber],
+                                [time.minsString, time.hoursString, time.daysString, time.monthsString, time.yearsString],
+                                setNotification
+                            )) {
+                                saveEvent(
+                                    timeToAdd, descriptionToAdd, setNotification,
+                                    id, events, setEvents, isClockEvents
+                                )
+                            }
                         }
                     }
                 >
--- src/helpers/clockevents.js
+++ src/helpers/clockevents.js
@@ -1,5 +1,6 @@
 // Helpers
 import { getNumberSuffix, capitalizeFirstLetter } from './misc'
+import { fireError } from './notifications'
 // Objects
 import { CLOCKEVENTUNITS } from './objects'
 
@@ -32,4 +33,42 @@ export const generateClockTimeStampWithoutClock = (year, month, day, timer) => {
         ("0" + Math.floor((timer / 60000) % 60)).slice(-2) + " on " +
         month + "/" + day + "/" + year
     )
+}
+
+export const translateTimeStampToMili = (hours, mins) => {
+    const hoursToMili = hours * 3600000
+    const minsToMili = mins * 60000
+
+    return hoursToMili + minsToMili
+}
+export const translateInputToFullTimeStamp = (timeStamp, days, months, years) => {
+    return ("[" + years + ":" + months + ":" + days + ":" + timeStamp + "]")
+}
+
+export const checkInput = (timeNumberArray, timeStringArray, setNotification) => {
+    let returnBool = true
+    timeNumberArray.forEach((time) => {
+        if (isNaN(time)) {
+            fireError(setNotification, "Bad User Input", "Illegal character, please only use numbers")
+            returnBool = false
+        }
+    })
+    timeStringArray.forEach((time, i) => {
+        if (i === (timeStringArray.length - 1)) {
+            if (!(/^[-\d]+$/.test(time))) {
+                fireError(setNotification, "Bad User Input", "Illegal character, please only use numbers")
+                returnBool = false
+            }
+        } else if (!(/^[\d]+$/.test(time))) {
+            fireError(setNotification, "Bad User Input", "Illegal character, please only use numbers")
+            returnBool = false
+        }
+    })
+    return returnBool
+}
+export const saveEvent = (
+    timeToAdd, descriptionToAdd, setNotification,
+    id, events, setEvents, isClockEvents
+) => {
+
 }
