ae6368d1051e72c20fe90af3a70ca46d684f41c2
Carson-Key <keyc@oregonstate.edu>
2022-06-10 14:30:49 -0700

    Update PopUp to better match website style

--- src/ui/ConfirmationPopUp.js
+++ src/ui/ConfirmationPopUp.js
@@ -3,20 +3,32 @@ const ConfirmationPopUp = (props) => {
 
     return (
         <>
-            <div className="z-40 absolute inset-0 grid justify-items-center">
-                <div className="bg-white w-1/3 h-1/3 mx-auto my-auto rounded-lg">
-                    <div className="h-2/3 flex justify-around">
-                        <div className="text-2xl my-auto">
-                            {message}
-                        </div>
-                    </div>
-                    <div className="flex justify-around h-1/3">
-                        <button className="h-8 my-auto text-white bg-red-500" onClick={cancel}>Cancel</button>
-                        <button className="h-8 my-auto test-white bg-green-500" onClick={onClick}>Confirm</button>
-                    </div>
+            <div className="
+                z-50 absolute top-1/2 left-1/2 
+                transform -translate-x-1/2 -translate-y-1/2 
+                w-72 bg-white h-fit py-4 px-2 rounded-lg
+            ">
+                <p className="text-center mb-4">
+                    {message}
+                </p>
+                <div className="flex justify-around h-1/3">
+                    <button 
+                        className="h-8 my-auto text-white bg-red-500 py-1 px-3 w-fit rounded" 
+                        onClick={cancel}
+                    >
+                        Cancel
+                    </button>
+                    <button 
+                        className="h-8 my-auto test-white text-white bg-green-500 py-1 px-3 w-fit rounded"
+                        onClick={onClick}
+                    >
+                        Confirm
+                    </button>
                 </div>
             </div>
-            <div className="fixed h-screen w-screen flex bg-black bg-opacity-25 z-0 inset-0"></div>
+            <div 
+                className="fixed h-screen w-screen flex bg-black bg-opacity-50 z-40 inset-0"
+            ></div>
         </>
     )
 }
