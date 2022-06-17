7c345a24299907a1f9e6afd347817e8944fa3fc4
Carson-Key <keyc@oregonstate.edu>
2022-06-17 13:13:39 -0700

    Fix hiding unused basic info not working

--- src/components/CharacterNotes/BasicInfo.js
+++ src/components/CharacterNotes/BasicInfo.js
@@ -25,17 +25,17 @@ const BasicInfo = (props) => {
                 condition={isAdmin}
                 returnComponent={
                     <div className="mt-1 mb-4">
-                        <ConditionalRender condition={name || name === ""}>
+                        <ConditionalRender condition={!(!name || name === "")}>
                             <div className="flex flex-wrap mx-2 gap-2">
                                 <h5 className="font-semibold">Name:</h5><p>{name}</p>
                             </div>
                         </ConditionalRender>
-                        <ConditionalRender condition={position || position === ""}>
+                        <ConditionalRender condition={!(!position || position === "")}>
                             <div className="flex flex-wrap mx-2 gap-2">
                                 <h5 className="font-semibold">Position:</h5><p>{position}</p>
                             </div>
                         </ConditionalRender>
-                        <ConditionalRender condition={status || status === ""}>
+                        <ConditionalRender condition={!(!status || status === "")}>
                             <div className="flex flex-wrap mx-2 gap-2">
                                 <h5 className="font-semibold">Status:</h5>
                                 <p className={"px-2 rounded " + generateStatusClasses(status)}>{status}</p>
