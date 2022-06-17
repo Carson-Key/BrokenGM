b915e01c2d8bf5a640985b0e7a01ca24b19627bb
Carson-Key <keyc@oregonstate.edu>
2022-06-17 13:12:03 -0700

    Fix notifiaction positioning

--- src/ui/Notification.js
+++ src/ui/Notification.js
@@ -2,11 +2,7 @@ const Notification = (props) => {
     const { message, className } = props
 
     return (
-        <div className="absolute top-14 lg:top-24 2xl:top-28 w-screen px-1">
-            <div className={"w-fit rounded-lg text-xl lg:text-3xl px-5 py-2 mt-1 mx-auto " + className}>
-                <p>{message}</p>
-            </div>
-        </div>
+        <p className={"absolute z-40 top-1 left-1/2 transform -translate-x-1/2 w-fit rounded-lg text-xl lg:text-3xl px-5 py-2 " + className}>{message}</p>
     )
 }
 
