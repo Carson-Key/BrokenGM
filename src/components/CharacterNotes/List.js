eac649ba3f51294dafd4eeb6e62d477db4758051
Carson-Key <keyc@oregonstate.edu>
2022-06-16 20:23:45 -0700

    Add update functionality to list

--- src/components/CharacterNotes/List.js
+++ src/components/CharacterNotes/List.js
@@ -1,13 +1,24 @@
 // Packages
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+// Character Notes
+import VariableTextArea from './VariableTextArea'
 // Components
 import ConditionalRender from '../ConditionalRender'
+// Contexts
+import { NotificationContext } from '../../contexts/Notification'
 // Helpers
 import { moreLessTextDecider } from '../../helpers/misc'
+import { updateDocument } from '../../helpers/firestore'
 
 const List = (props) => {
-    const { isAdmin, list, name } = props
+    const { 
+        isAdmin, name, list, setNotes, index, 
+        notes, isCharacterNotes, id, elementIndex
+    } = props
     const [expandList, setExpandList] = useState((list.length > 5) ? false : true)
+    const [listState, setListState] = useState([...list])
+    const [resetInputValues, setResetInputValues] = useState(false)
+    const setNotification = useContext(NotificationContext)[1]
 
     return (
         <div>
@@ -26,15 +37,48 @@ const List = (props) => {
                     }
                 >
                     <div className="flex flex-col w-full px-2 break-words">
-                        {list.map((element, i) => {
-                            return (<p key={i} className="my-2">{element}</p>)
+                        {listState.map((element, i) => {
+                            return (
+                                <VariableTextArea
+                                    value={element}
+                                    placeholder={"Change the Value of this Item"}
+                                    index={i}
+                                    values={listState} 
+                                    setValue={setListState}
+                                    changeValues={(tempValues, index, inputValue) => {
+                                        tempValues[index] = inputValue
+                                    }}
+                                    resetInputValues={resetInputValues}
+                                    setResetInputValues={setResetInputValues}
+                                />
+                            )
                         })}
                     </div>
                 </ConditionalRender>
             </ConditionalRender>
-            <button className="flex text-lg text-blue-500 my-2" 
-                onClick={() => {setExpandList(!expandList)}
-            }>{moreLessTextDecider(expandList)}</button>
+            <div className={"w-full flex mt-2 py-2 " + (expandList ? "justify-between" : "justify-end")}>
+                <button 
+                    disabled={!expandList}
+                    className={
+                        expandList ?
+                        "bg-green-500 text-white rounded px-2 py-1" :
+                        "hidden"
+                    }
+                    onClick={() => {
+                        let tempNotes = [...notes]
+                        tempNotes[index][elementIndex] = {
+                            ...tempNotes[index][elementIndex],
+                            list: listState
+                        }
+                        setNotes(tempNotes)
+                        setResetInputValues(0)
+                        updateDocument("characternotes", id, {characters: tempNotes}, setNotification, isCharacterNotes)
+                    }}
+                >Update</button>
+                <button className="flex text-lg text-blue-500" 
+                    onClick={() => {setExpandList(!expandList)}
+                }>{moreLessTextDecider(expandList)}</button>
+            </div>
         </div>
 	)
 }
---
+++ src/components/CharacterNotes/VariableTextArea.js
@@ -0,0 +1,33 @@
+// Packages
+import { useState, useEffect } from 'react'
+
+const VariableTextArea = (props) => {
+    const { 
+        value, placeholder, index, values, 
+        setValue, changeValues, resetInputValues, setResetInputValues
+    } = props
+    const [inputValue, setInputValue] = useState("")
+
+    useEffect(() => {
+        if (resetInputValues < values.length) {
+            setInputValue("")
+            setResetInputValues(prev => prev + 1)
+        }
+    }, [resetInputValues, values, setResetInputValues])
+
+    return (
+        <textarea
+            className="border rounded px-2 py-1 my-2 w-full"
+            placeholder={value || value !== "" ? value : placeholder}
+            value={inputValue}
+            onChange={(event) => {
+                let tempValues = [...values]
+                changeValues(tempValues, index, event.target.value)
+                setValue(tempValues)
+                setInputValue(event.target.value)
+            }}
+        />
+	)
+}
+
+export default VariableTextArea
