f6b50ab62f2604395619a3bdbb501c47fca921ca
Carson-Key <keyc@oregonstate.edu>
2022-07-01 10:53:27 -0700

    Remove clear list/named list values on update

--- src/components/CharacterNotes/VariableInput.js
+++ src/components/CharacterNotes/VariableInput.js
@@ -1,19 +1,12 @@
 // Packages
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 const VariableInput = (props) => {
     const { 
         value, placeholder, index, values, 
-        setValue, changeValues, resetInputValues, setResetInputValues
+        setValue, changeValues
     } = props
     const [inputValue, setInputValue] = useState("")
-
-    useEffect(() => {
-        if (resetInputValues < values.length) {
-            setInputValue("")
-            setResetInputValues(prev => prev + 1)
-        }
-    }, [resetInputValues, values, setResetInputValues])
 
     return (
         <input
