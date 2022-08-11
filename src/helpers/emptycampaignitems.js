1dcecf3561086228164d5c8437b088f1c8a47205
Carson-Key <keyc@oregonstate.edu>
2022-08-11 13:29:46 -0700

    Add empty campaign items object

---
+++ src/helpers/emptycampaignitems.js
@@ -0,0 +1,50 @@
+export const CHARACTERNNOTES = {
+    admins: [],
+    characters: [],
+    name: "",
+    players: []
+}
+export const CLOCKEVENTS = {
+    admins: [],
+    clock: "",
+    events: [],
+    name: "",
+    players: []
+}
+export const CLOCKS = {
+    admins: [],
+    clockEvent: "",
+    dayOfMonth: 0,
+    dayOfWeek: 0,
+    daysInMonths: [],
+    daysInWeek: [],
+    hoursInDay: 0,
+    isActive: false,
+    monthOfYear: 0,
+    monthsOfYear: [],
+    name: "",
+    players: [],
+    preYearSuffix: "",
+    timer: 0,
+    year: 0,
+    yearSuffix: ""
+}
+export const RELATIONS = {
+    admins: [],
+    names: "",
+    npcs: [],
+    playerCharacters: [],
+    players: []
+}
+export const VOTINGSYSTEMSFIRESTORE = {
+    admins: [],
+    name: "",
+    players: [],
+    "realtime-id": ""
+}
+export const VOTINGSYSTEMSREALTIME = {
+    admins: {},
+    defaultVoters: {},
+    voters: {},
+    votes: {}
+}
